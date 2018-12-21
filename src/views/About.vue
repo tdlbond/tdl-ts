ce573d0d480031a0b9dc83dd3c300f8923dfa38d
tangdalu <tangdl134@163.com>
2018-12-21 20:40:28 +0800

    add a word

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2 v-text="message"></h2>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      message: 'hello world!!!'
+    }
+  }
+}
+</script>
+
