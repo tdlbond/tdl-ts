8192b2d6f6a1b7e4087013d71ca45de3bc5e82e9
tangdalu <tangdl134@163.com>
2018-12-21 21:21:36 +0800

    edit the word

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <h2 v-text="message"></h2>
+    <h2 v-text="messages"></h2>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      message: 'hello world!!!'
+      messages: 'hello world!!!'
     }
   }
 }
